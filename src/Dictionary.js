b5fb04b4b0836c8fa0a00aa2c311f2416797aacd
bjohne <97284793+bjohne@users.noreply.github.com>
2023-02-27 17:13:57 +0100

    going back in code because deploying problems

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
-import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0].definition);
-    setResults(response.data);
+    console.log(response.data);
   }
 
   function search(event) {
@@ -30,7 +27,6 @@ export default function Dictionary() {
         <input type="search" onChange={handleKeywordChange} />
       </form>
       <br />
-      <Results results={results} />
     </div>
   );
 }
